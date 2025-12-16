82022d60481fc67aab15067ed1da146e0c072aaf
BmG-Dev104 <unusualbmg@gmail.com>
2025-12-16 06:01:54 -0800

    made update on task delete button

--- script.js
+++ script.js
@@ -1,107 +1,153 @@
-//selections for tabs
-const tabAll = document.querySelector(".tab1")
-const tabActive = document.querySelector(".tab2")
-const tabCompleted = document.querySelector(".tab3")
-//selections for tab buttons
-const tabBtn1 = document.querySelector(".tabBtn1")
-const tabBtn2 = document.querySelector(".tabBtn2")
-const tabBtn3 = document.querySelector(".tabBtn3")
-//selections for input element and add button
+//second try
+const tasktap1 = document.querySelector(".tab1")
+const tasktap2 = document.querySelector(".tab2")
+const tasktap3 = document.querySelector(".tab3")
+const Btn1 = document.querySelector(".tabBtn1")
+const Btn2 = document.querySelector(".tabBtn2")
+const Btn3 = document.querySelector(".tabBtn3")
 const inputEl = document.querySelector(".inputEl")
 const addBTN = document.querySelector(".addBTN")
+const lightDark = document.querySelector(".lightDark")
+const ClearBTN = document.querySelector(".ClearBTN")
+const exportBTN = document.querySelector(".exportBTN")
+const importBTN = document.querySelector(".importBTN")
 
 
+//making the first tab to  show
+tasktap1.classList.add("showTab");
 
-//show the  first tab on initial load
-tabAll.classList.add("showTab")
+//able different tabs to show while on the page
+Btn1.addEventListener("click", function () {
+    tasktap1.classList.add("showTab")
+    tasktap2.classList.remove("showTab")
+    tasktap3.classList.remove("showTab")
+})
+Btn2.addEventListener("click", function () {
+    tasktap1.classList.remove("showTab")
+    tasktap2.classList.add("showTab")
+    tasktap3.classList.remove("showTab")
 
-
-tabBtn1.addEventListener("click", function () {
-    tabAll.classList.add("showTab")
-    tabActive.classList.remove("showTab")
-    tabCompleted.classList.remove("showTab")
+})
+Btn3.addEventListener("click", function () {
+    tasktap1.classList.remove("showTab")
+    tasktap2.classList.remove("showTab")
+    tasktap3.classList.add("showTab")
 
 })
 
-tabBtn2.addEventListener("click", function () {
-    tabAll.classList.remove("showTab")
-    tabActive.classList.add("showTab")
-    tabCompleted.classList.remove("showTab")
-
-
-})
-tabBtn3.addEventListener("click", function () {
-    tabAll.classList.remove("showTab")
-    tabActive.classList.remove("showTab")
-    tabCompleted.classList.add("showTab")
-
-});
-function renderTasks(userinput) {
-    //Creating task left side
-    let checkbox = document.createElement("input")
-    checkbox.type = "checkbox"
+function renderTasks(userinput, id) {
+    console.log(userinput)
+    if(userinput. length<1){
+        return
+    }
+    let inputbox = document.createElement("input")
+    inputbox.type = "checkbox"
     let pElement = document.createElement("p")
     pElement.textContent = userinput
-    let taskElement = document.createElement("div")
-    taskElement.classList.add("taskleftside")
+    let divbox1 = document.createElement("div")
+    divbox1.classList.add("taskleftside")
+    divbox1.appendChild(inputbox)
+    divbox1.appendChild(pElement)
 
-    taskElement.appendChild(checkbox)
-    taskElement.appendChild(pElement)
+    let buttonbox1 = document.createElement("button")
+    buttonbox1.classList.add("controlBTN")
+    buttonbox1.classList.add("editbutton");
+    buttonbox1.textContent = "✎"
+    buttonbox1.onclick = edittask
+    let buttonbox2 = document.createElement("button")
+    buttonbox2.classList.add("controlBTN")
+    buttonbox2.classList.add("deletebutton")
+    buttonbox2.textContent = "🗑"
+    buttonbox2.onclick = deletetask
 
-    console.log(taskElement)
+    let divbox2 = document.createElement("div")
+    divbox2.classList.add("taskRightside")
+    divbox2.appendChild(buttonbox1)
+    divbox2.appendChild(buttonbox2)
 
-    //creating taskrightside
-    let writebTN = document.createElement("button")
-    writebTN.classList.add("controlBTN")
-    writebTN.classList.add("editbutton")
-    writebTN.textContent = "✎"
-    let deletebTN = document.createElement("button")
-    deletebTN.classList.add("controlBTN")
-    deletebTN.classList.add("editbutton")
-    deletebTN.textContent = "🗑"
+    let genDIV = document.createElement("div")
+    genDIV.classList.add("taskchild")
+    genDIV.id = id
+    genDIV.appendChild(divbox1)
+    genDIV.appendChild(divbox2)
 
-    let divtask = document.createElement("div")
-    divtask.classList.add("taskRightside")
-    divtask.appendChild(writebTN)
-    divtask.append(deletebTN)
-    console.log(divtask)
-    //TAsk CHILD DIv
-    let taskchidDIV = document.createElement("div")
-    taskchidDIV.classList.add("taskchild")
-    taskchidDIV.appendChild(taskElement)
-    taskchidDIV.appendChild(divtask)
-    tabAll.appendChild(taskchidDIV)
+    tasktap1.appendChild(genDIV)
 
 
 }
+//empty arry that will hold all tasks text
+let tasksarry = [];
 
-let taskArray = [ ]
-
-
-
-function addTask() {
+function addtask() {
+    //Get the text that the user typed in the input
     let userinputvalue = inputEl.value;
-    taskArray.push(userinputvalue);
-    //clearin the input
+    //Add this new task to our  tasksarry
+    console.log(tasksarry)
+    tasksarry.push(userinputvalue);
+    console.log(tasksarry)
+    //clear the input box  so it`s empty and ready for the next task
     inputEl.value = "";
-    console.log(taskArray);
-//clearing exiting html
-tabAll.innerHTML="";
-
-    //
-    for (let i=0;i<taskArray.length;i++){
-renderTasks(taskArray[i])
+    //log the array to  see all tasks(for debugging)
+    //clearing all existing tasks from  the "All Tasks"tab
+    //(we`re going to redraw everything from scratch)
+    tasktap1.innerHTML = "";
+    //Loop through every task in the array
+    for (let i = 0; i < tasksarry.length; i++) {
+        //for each task, call renderTask()to create and display it
+        renderTasks(tasksarry[i], i);
     }
-
 }
+
+//when the "Add Task"button is clicked ,run the addtask function
 addBTN.addEventListener("click", function () {
-    addTask();
+    addtask();
 });
 
+//when a key is pressed while the input box is focused 
 inputEl.addEventListener("keypress", function (e) {
+    //if the key pressed was "Enter"
     if (e.key === "Enter") {
-        addTask();
-
+        addtask();//Run the addTask function
     }
-});
+})
 
+
+function edittask(e) {
+    // alert("Edit Task Functionality coming soon")
+    let editbutton = e.target
+    let taskRightside = editbutton.parentElement
+    let taskchild = taskRightside.parentElement
+}
+
+
+function deletetask(e) {
+    //alert("Delete Task Functionality coming soon!")
+    let deletebutton = e.target
+    let taskRightside = deletebutton.parentElement
+    let taskchild = taskRightside.parentElement
+    //DELETE 
+    let id = taskchild.id
+    tasksarry.splice(id, 1)
+    addtask()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+function imposters() {
+    alert("make sure you arent doing anything against time")
+}
+ClearBTN.addEventListener("click",function(){
+    tasksarry=[]
+    addtask()
+
+    
+})
